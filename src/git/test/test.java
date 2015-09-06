38848ebcfa7375116f3f81d5bc3873124d485ab7
Vitor <vspvitor@hotmail.com>
2015-09-06 00:10:39 -0300

    teste de conversão de pasta comum para projeto 2

---
+++ src/git/test/test.java
@@ -0,0 +1,13 @@
+package git.test;
+public class test {
+
+	public test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
